54dea595aa8e170c80163c7d6ebcc99dd5633a3e
justinbourb <justinbourb@gmail.com>
2021-09-20 11:10:59 -0700

    end of tutorial

--- src/index.js
+++ src/index.js
@@ -34,64 +34,15 @@ with the parent component.
 
 // create the tic-tac-toe board
 class Board extends React.Component {
-    /*
-    this.state = {squares} will store the state of each square, intialized as null
-    this.xIsNext: keeps track of the turns in the game
-     */
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true,
-        };
-    }
-
-    handleClick(i) {
-        // create a copy of the squares array instead of modifying the original
-        /*
-        Benefits of immutability:
-            1) Complex features become simple (detecting changes in data)
-                a) mutable data requires checking the entire data set for changes
-                b) if the immutable object does not equal the copy, then the object has changed
-            2) Determining when to re-render in React
-                a) immutable components are considered "pure components" in React.  Once a change is detected,
-                    the components can be re-rendered.
-                b) using built-in function shouldComponentUpdate()
-         */
-        /*
-        The const declaration creates a read-only reference to a value. It does not mean the value it holds is
-        immutable—just that the variable identifier cannot be reassigned.
-         */
-        const squares = this.state.squares.slice();
-        //exit function if there is a winner or the square is already filled
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        //toggle placing and X or an O based on xIsNext
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares:squares,
-            // toggle xIsNext true/false to track game moves
-            xIsNext: !this.state.xIsNext,
-        });
-    }
     renderSquare(i) {
         // returns the values from the squares Array, expect X, O, null
         // passes down props "this.state.value and this.props.value from Board to Square
-        return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)}/>;
+        return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)}/>;
     }
 
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status = '';
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -112,46 +63,105 @@ class Board extends React.Component {
     }
 }
 
-function calculateWinner(squares) {
-    /*
-    This function will calculate the winner of tic-tac-toe.
-    cont lines stores all the possible winning combinations of tic-tact-toe
-     */
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    // loop through all winning combinations to check for a winner
-    for (let i=0; i<lines.length; i++) {
-        const [a, b, c] = lines[i];
-        // check if the first square is filled a
-        // && checks if squares b and c match a
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            // if yes there is a winner
-            return squares[a];
-        }
-    }
-    // else return null - no winner yet
-    return null;
-}
-
 // creates a game
 class Game extends React.Component {
+    /*
+    this.history will store the array containing moves, one array stored for each move taken
+    this.state = {squares} will store the state of each square, intialized as null
+    this.xIsNext: keeps track of the turns in the game
+     */
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            stepNumber: 0,
+            xIsNext: true,
+        };
+    }
+
+    handleClick(i) {
+        // create a copy of the squares array instead of modifying the original
+        /*
+        Benefits of immutability:
+            1) Complex features become simple (detecting changes in data)
+                a) mutable data requires checking the entire data set for changes
+                b) if the immutable object does not equal the copy, then the object has changed
+            2) Determining when to re-render in React
+                a) immutable components are considered "pure components" in React.  Once a change is detected,
+                    the components can be re-rendered.
+                b) using built-in function shouldComponentUpdate()
+         */
+        /*
+        The const declaration creates a read-only reference to a value. It does not mean the value it holds is
+        immutable—just that the variable identifier cannot be reassigned.
+         */
+
+        // if the user clicks on one of the history buttons
+        // any history after that point will be dropped
+        const history = this.state.history.slice(0, this.state.stepNumber+1);
+        const current = history[history.length-1];
+        const squares = current.squares.slice();
+        //exit function if there is a winner or the square is already filled
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        //toggle placing and X or an O based on xIsNext
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            // .concat() is used in a similar manner to .push(), however it does not mutate the original array
+            history: history.concat([{
+                squares:squares,
+            }]),
+            stepNumber: history.length,
+            // toggle xIsNext true/false to track game moves
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        })
+    }
     render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+        /*
+        Using the map method, we can map our history of moves to React elements representing buttons on the screen,
+        and display a list of buttons to “jump” to past moves.
+         */
+        const moves = history.map((step, move) => {
+            const desc = move ? 'Go to move #' + move : 'Go to game start';
+            return (
+                /*
+                key is a special and reserved property in React (along with ref, a more advanced feature).
+                React uses the key property to create or destroy components on updates
+                 */
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
+        let status;
+        // check the game status, Winner, X turn or O turn
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board squares={current.squares} onClick={(i) => this.handleClick(i)}/>
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
@@ -195,3 +205,31 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
+function calculateWinner(squares) {
+    /*
+    This function will calculate the winner of tic-tac-toe.
+    cont lines stores all the possible winning combinations of tic-tact-toe
+     */
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    // loop through all winning combinations to check for a winner
+    for (let i=0; i<lines.length; i++) {
+        const [a, b, c] = lines[i];
+        // check if the first square is filled a
+        // && checks if squares b and c match a
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            // if yes there is a winner
+            return squares[a];
+        }
+    }
+    // else return null - no winner yet
+    return null;
+}
